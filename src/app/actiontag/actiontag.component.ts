eb7db88bfd4c868a20680d9c4e32376de1aef150
Baji65 <baji.katari1@gmail.com>
2025-10-25 14:52:55 +0530

    25-10-2025

--- src/app/actiontag/actiontag.component.ts
+++ src/app/actiontag/actiontag.component.ts
@@ -10,168 +10,135 @@ import { SiteService } from 'src/services/site.service';
 @Component({
   selector: 'app-actiontag',
   templateUrl: './actiontag.component.html',
-  styleUrls: ['./actiontag.component.css']
+  styleUrls: ['./actiontag.component.css'],
 })
 export class ActiontagComponent {
-
-  fields:any[]=[
+  fields: any[] = [
     {
-      serial:0,
-      label:"S.no",
-     showSerial:true
+      serial: 0,
+      label: 'S.no',
+      showSerial: true,
     },
     {
-      serial:1,
-      label:"Site/Camera Id",
-      id:"cameraName",
-      sort:true,
-    
-     
+      serial: 1,
+      label: 'Site/Camera Id',
+      id: 'cameraName',
+      sort: true,
     },
     {
-      serial:2,
-      label:"Action Tags",
-      id:"value",
-      sort:true,
-    }
-  ]
+      serial: 2,
+      label: 'Action Tags',
+      id: 'value',
+      sort: true,
+    },
+  ];
   actionTags: any;
 
+  constructor(
+    private siteser: SiteService,
+    private metadaSer: MetadataService,
+    private alert: AlertService
+  ) {}
 
-constructor(private siteser :SiteService,
-  private metadaSer:MetadataService,
-  private alert : AlertService
-){
-
-}
-
-
-ngOnInit(){
-this.getTypes();
-if(this.details.type=='view'){
-
-  this.getActiontags();
-}
-}
-optionselect: string="Site"
-renderoption: string[] = ['Site', 'Cameras'];
-  @Input() cameralist!:any;
-  @Input() monitoringCameras!:any;
-  @Input() details!:any;
-  deviceName:any;
- announcer = inject(LiveAnnouncer);
-
-    
-    deviceSearch: any;
-    currentCamera:any;
-    searchDevices(event: any) {
-      this.deviceSearch = (event.target as HTMLInputElement).value;
+  ngOnInit() {
+    this.getTypes();
+    if (this.details.type == 'view') {
+      this.getActiontags();
     }
+  }
+  optionselect: string = 'Site';
+  renderoption: string[] = ['Site', 'Cameras'];
+  @Input() cameralist!: any;
+  @Input() monitoringCameras!: any;
+  @Input() details!: any;
+  deviceName: any;
+  announcer = inject(LiveAnnouncer);
 
-    
-  filterCam(type:any){
-
-    this.currentCamera=type;
-
+  deviceSearch: any;
+  currentCamera: any;
+  searchDevices(event: any) {
+    this.deviceSearch = (event.target as HTMLInputElement).value;
   }
 
-  
-    separatorKeysCodes: number[] = [ENTER, COMMA];
-    toCtrl = new FormControl('', Validators.required);
-    to: string[] = [];
-     @ViewChild('fruitInput') fruitInput !: ElementRef<HTMLInputElement>;
+  filterCam(type: any) {
+    this.currentCamera = type;
+  }
+
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  toCtrl = new FormControl('', Validators.required);
+  to: string[] = [];
+  @ViewChild('fruitInput') fruitInput!: ElementRef<HTMLInputElement>;
 
   remove(fruit: string, type: string): void {
-    
-    if(type =='to') {
+    if (type == 'to') {
       const index = this.to.indexOf(fruit);
       if (index >= 0) {
         this.to.splice(index, 1);
         this.announcer.announce(`Removed ${fruit}`);
       }
-    } 
-    
+    }
   }
-    add(event: MatChipInputEvent): void {
-      const value = (event.value || '').trim();
-  
-      if(value){
-  
-        if (event.input.name == 'to' && !this.to.includes(value)) {
-          this.to.push(value);
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    if (value) {
+      if (event.input.name == 'to' && !this.to.includes(value)) {
+        this.to.push(value);
+      }
+    }
+    event.chipInput!.clear();
+  }
+  actionTag: any;
+
+  createActionTag() {
+    let payload = {
+      siteId: this.details.item.siteId,
+      cameraActionTags: [
+        {
+          cameraId: this.deviceName,
+          actionTagId: this.actionTag,
+        },
+      ],
+    };
+
+    if (this.details.item.siteId && this.actionTag) {
+      this.siteser.createActionTag(payload).subscribe((res: any) => {
+        if (res.statusCode == 200) {
+          this.alert.snackSuccess(res.message);
+          this.deviceName = null;
+          this.actionTag = null;
+        } else {
+          this.alert.snackError(res.message);
         }
-         
-      }
-      event.chipInput!.clear();
-    }
-    actionTag:any;
-
-    createActionTag(){
-
-      let payload={
-        siteId: this.details.item.siteId,
-        cameraActionTags: [
-          {
-            cameraId: 
-              this.deviceName
-            ,
-            actionTagId: 
-              this.actionTag
-            
-          }
-        ]
-      }
-
-      if(this.details.item.siteId && this.actionTag){
-
-        this.siteser.createActionTag(payload).subscribe((res:any)=>{
-
-          if(res.statusCode==200){
-
-            this.alert.snackSuccess(res.message);
-            this.deviceName=null;this.actionTag=null;
-          }
-          else{
-            this.alert.snackError(res.message);
-          }
-        })
-      }
-      else{
-
-        this.alert.snackError("Please select Action Tag")
-      }
-    
-    }
-    
-    getTypes() {
-
-      this.metadaSer.getMetadata().subscribe((res: any) => {
-  
-        res.forEach((item: any) => {
-  
-          if (item.typeName === "Action_Tag") {
-            this.actionTags = item.metadata;
-          }
-        
-        });
       });
-  
+    } else {
+      this.alert.snackError('Please select Action Tag');
     }
-actionTagData:any;
-    getActiontags(){
+  }
 
-      this.siteser.getlistActionTags_1_0(this.details.item.siteId).subscribe((res:any)=>{
-       if(res.statusCode==200){
+  getTypes() {
+    this.metadaSer.getMetadata().subscribe((res: any) => {
+      res.forEach((item: any) => {
+        if (item.typeName === 'Action_Tag') {
+          this.actionTags = item.metadata.sort((a: any, b: any) =>
+            a.value.localeCompare(b.value)
+        );
+      }
 
-         this.actionTagData=res.data[0].actionTags;
-
-       }
-       else{
-        this.actionTagData=[];
-        // this.alert.snackError(res.message);
-       }
-
-      })
-    }
-  
+      });
+    });
+  }
+  actionTagData: any;
+  getActiontags() {
+    this.siteser
+      .getlistActionTags_1_0(this.details.item.siteId)
+      .subscribe((res: any) => {
+        if (res.statusCode == 200) {
+          this.actionTagData = res.data[0].actionTags;
+        } else {
+          this.actionTagData = [];
+          // this.alert.snackError(res.message);
+        }
+      });
+  }
 }
